169097f09cc6fdbde74d87965f9c71caaf7ed824
ZephyrHub1 <quanghaha10092008@gmail.com>
2025-09-05 19:41:24 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,2 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/veryimportantrr/x/refs/heads/main/gag_visual.lua", true))("discord.gg/csxu2nCkw9")
+local Spawner = loadstring(game:HttpGet("https://gitlab.com/darkdark/dark/-/raw/main/Spawner.lua"))()
+Spawner.Load()
 loadstring(game:HttpGet("https://pastefy.app/4XVrquep/raw"))()
