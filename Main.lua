c20be55280e24361dbe65fa0f280fa07152fb186
ZephyrHub1 <quanghaha10092008@gmail.com>
2025-10-02 23:38:29 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,3 @@
 ID="b5bd87ab-c631-494c-9b65-aa0f5b823563";
 loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ZusumeHub/ZusumeHub/refs/heads/main/UpdateZysumee"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
